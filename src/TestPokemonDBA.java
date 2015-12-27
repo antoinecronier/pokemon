40f66a16585e093b3e9b958a786d77bbcd85cdd7
Antoine Cronier <antoine.cronier@tactfactory.com>
2015-12-27 20:13:49 +0100

    add junit test

---
+++ src/TestPokemonDBA.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestPokemonDBA {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertFalse(false);
+	}
+
+}
